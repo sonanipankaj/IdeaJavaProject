6838855fed704a9936eda6e225717c822d517a2a
Pankaj <sonani_pankaj@hotmail.com>
2016-04-20 12:30:27 -0500

    first commit with git ignore file and TestClass.java

---
+++ CoreJava/src/TestCalss.java
@@ -0,0 +1,8 @@
+/**
+ * Created by pankajs on 4/20/2016.
+ */
+public class TestCalss {
+    public static void main(String[] args) {
+        System.out.println("This is first Test class for application");
+    }
+}
