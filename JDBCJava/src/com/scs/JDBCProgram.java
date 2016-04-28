3737223c51789935bb57f1d0cbb78ccdc6139236
Pankaj <sonani_pankaj@hotmail.com>
2016-04-28 08:51:59 -0500

    Added JDBC module and text file for theori

---
+++ JDBCJava/src/com/scs/JDBCProgram.java
@@ -0,0 +1,15 @@
+package com.scs;
+
+import javax.swing.JOptionPane;
+import java.sql.*;
+
+public class JDBCProgram {
+
+    static String userid = "root", password = "x0we0ps";
+    static String url = "jdbc:mysql://localhost:3306/jdbc_project";
+    static Statement stmt;
+    static PreparedStatement pstmt;
+    static Connection con;
+
+  
+}//End of class
