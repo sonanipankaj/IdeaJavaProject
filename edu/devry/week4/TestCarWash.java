9b5930b2d6164a151eff5d775b79cf374c88aa1e
Pankaj <sonani_pankaj@hotmail.com>
2016-05-25 15:34:05 -0500

    Added TestCar java file

---
+++ edu/devry/week4/TestCarWash.java
@@ -0,0 +1,11 @@
+package devry.week4;
+
+/**
+ * Created by pankajs on 5/25/2016.
+ */
+public class TestCarWash {
+
+    public static void main(String[] args) {
+        System.out.println("Hi from week 4");
+    }
+}
